90ac0180f2eb5cf929e10164d9b9e0bed845c620
Rebeca R Donato <rebecardonato@gmail.com>
2024-02-18 23:00:31 -0300

    Adicionadas funções de crédito e débito

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Primeiro = limite - Segundo = saldo
+var clientes = map[string][]int{
+	"1": {100000, 0},
+	"2": {80000, 0},
+	"3": {1000000, 0},
+	"4": {10000000, 0},
+	"5": {500000, 0},
+	"7": {10, 0},
+}
+
 func main() {
 	ws := gin.Default()
 	ws.GET("/clientes/:id/extrato", getExtrato)
@@ -15,16 +25,61 @@ func main() {
 
 func postTransacao(ctx *gin.Context) {
 	type postTransacaoResposta struct {
+		Limite int `json:"limite"`
+		Saldo  int `json:"saldo"`
+	}
+
+	type postTransacaoRequestBody struct {
 		Valor     int    `json:"valor"`
 		Tipo      string `json:"tipo"`
 		Descricao string `json:"descricao"`
 	}
-	resposta := postTransacaoResposta{
-		Valor:     1000,
-		Tipo:      "c",
-		Descricao: "descricao",
+
+	id := ctx.Param("id")
+	cliente, ok := clientes[id]
+
+	if ok == false {
+		ctx.Status(404)
+		return
 	}
+
+	requestBody := postTransacaoRequestBody{}
+
+	err := ctx.BindJSON(&requestBody)
+	if err != nil {
+		ctx.Status(422)
+		return
+	}
+
+	if requestBody.Tipo != "c" && requestBody.Tipo != "d" {
+		ctx.Status(422)
+		return
+	}
+
+	if len(requestBody.Descricao) < 1 || len(requestBody.Descricao) > 10 {
+		ctx.Status(422)
+		return
+	}
+
+	if requestBody.Tipo == "c" {
+		cliente[1] = cliente[1] + requestBody.Valor
+	}
+
+	if requestBody.Tipo == "d" {
+		if cliente[0]+cliente[1] == 0 {
+			ctx.Status(422)
+			return
+		}
+		cliente[1] = cliente[1] - requestBody.Valor
+	}
+
+	resposta := postTransacaoResposta{
+		Limite: cliente[0],
+		Saldo:  cliente[1],
+	}
+
 	ctx.JSON(http.StatusOK, resposta)
+
 }
 
 func getExtrato(ctx *gin.Context) {
